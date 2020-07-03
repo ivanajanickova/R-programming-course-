b8ef67877dec799d32039f5725fc54f728becfa8
Ivana Janickova <67543976+ivanajanickova@users.noreply.github.com>
2020-07-03 08:09:58 +0200

    Update corr.R

--- corr.R
+++ corr.R
@@ -1,14 +1,15 @@
+#This function woks with data from monitoring sufate and nitrate levels on 332 monitors
+##It takes directory of a file and treshold value for complete cases a  monitor file (001.csv - 332.csv) has to have
+###The function returns correlations between sulfate and nitrate levels for each monitor
 corr <- function(directory, treshold = 0) {
-  sulfate <- 0
-  nitrate <- 0
   correlation <-0
   complete_cases <- complete(directory = "specdata")
   for(i in 1 : nrow(complete_cases)){
-    if(complete_cases$nobs[i] > treshold) {
+    if(complete_cases$nobs[i] > treshold) {  #chceck if the complete cases meet the treshold requirement
       if(complete_cases$id[i] < 10) {
-        csv <- read.csv(paste0("00", complete_cases$id[i], ".csv"))
-        no_na <- na.omit(csv)
-        correlation<- c(correlation, cor(no_na$sulfate, no_na$nitrate))
+        csv <- read.csv(paste0("00", complete_cases$id[i], ".csv"))  #loading the csv file 
+        no_na <- na.omit(csv)  #getting rid of NA values
+        correlation<- c(correlation, cor(no_na$sulfate, no_na$nitrate)) #adding the value to the correlation vector
       }
       else if(complete_cases$id[i] >= 10 && complete_cases$id[i] < 100) {
         csv <- read.csv(paste0("0", complete_cases$id[i], ".csv"))
@@ -19,10 +20,10 @@ corr <- function(directory, treshold = 0) {
       else {
         csv <- read.csv(paste0(complete_cases$id[i], ".csv"))
         no_na <- na.omit(csv)
-        correlation<- c(correlation, cor(no_na$sulfate, no_na$nitrate))
+        correlation<- c(correlation, cor(no_na$sulfate, no_na$nitrate)) 
         
       }
     }
   }
-  return (correlation[2 : length(correlation)])
+  return (correlation[2 : length(correlation)]) #vector of correlations for monitors that meet treshold requuirement
 }
